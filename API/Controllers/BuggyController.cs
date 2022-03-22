c6d3602612dd39155402f1c785967d9756a9f0b9
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2022-03-22 16:36:54 +0530

    middleware exception class created for server error

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("Bad Request");
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnAuthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("name", "name is required");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("THis is servier error");
+        }
+    }
+}
